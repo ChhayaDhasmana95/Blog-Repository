cdea1716a34f49fc16ab28f80e4ad8aefaf91c8b
Chhaya <chhayadhasmana95@gmail.com>
2018-09-02 01:59:11 +0530

    added post controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+
+	protected
+	# def after_sign_in_path_for(resource)
+ #      new_user_registration_path
+ #    end
+
+    def after_sign_up_path_for(resource)
+      new_user_session_path(resource)
+    end
+
+    # protected
+    # def after_sign_up_path_for(resource)
+    #   signed_in_root_path(resource)
+    # end
+
+    # def after_update_path_for(resource)
+    #   signed_in_root_path(resource)
+    # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+ 
   devise_for :users
+  namespace :devise, :path => "/" do
+    #Registrations
+    post 'sign_up' => 'registrations#create' 
+
+    #Sessions
+    get 'login' => 'sessions#new'
+    post 'login'  => 'sessions#create'
+
+    
+  end
+  
+  post '/index' => 'posts#index'
+  root to: 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
