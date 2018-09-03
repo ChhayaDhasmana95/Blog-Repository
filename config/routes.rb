858f29fbdcd30b324bdcf1b2dd716e9533391f88
Chhaya <chhayadhasmana95@gmail.com>
2018-09-03 09:38:27 +0530

    dependent destroy added to models

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
   end
   
   get '/index' => 'posts#index'
-  root to: 'posts#index'
+  
 
   resources :posts do
     resources :comments
@@ -30,5 +30,5 @@ Rails.application.routes.draw do
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'posts#index'
 end
